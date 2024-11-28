d55be6295760a4d0b4cf1f1d598d1f134a2d8817
diegosorianom <diegosorianomagallon@gmail.com>
2024-11-28 18:35:37 +0100

    28-11-2024 18-35 comprasV2

--- compras/src/Cajera.java
+++ compras/src/Cajera.java
@@ -1,34 +1,33 @@
-public class Cajera {
+public class Cajera extends Thread {
     private int id;
+    private Cliente cliente;
+    private long initialTime;
 
-    public Cajera(int id) {
+    public Cajera(int id, Cliente cliente, long initialTime) {
         this.id = id;
+        this.cliente = cliente;
+        this.initialTime = initialTime;
     }
 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public long procesarCompra(Cliente cliente, long initialTime) {
-        System.out.println("La cajera ha comenzado con el cliente: " + cliente.getId() + " en el tiempo: " + (initialTime / 1000) + " segundos");
+    @Override
+    public void run() {
+        System.out.println("La cajera " + id + " ha comenzado con el cliente: " + cliente.getId()
+                + " en el tiempo: " + ((System.currentTimeMillis() - initialTime) / 1000) + " segundos");
 
         for (int i = 0; i < cliente.getCarroCompra().length; i++) {
             int productoId = i + 1;
-            System.out.println("La cajera escanea el producto: " + productoId);
+            System.out.println("La cajera " + id + " escanea el producto: " + productoId
+                    + " del cliente: " + cliente.getId());
             int tiempoEscaneo = cliente.getCarroCompra()[i];
 
             try {
-                Thread.sleep(tiempoEscaneo * 1000); // Convertir segundos a milisegundos
+                Thread.sleep(tiempoEscaneo * 1000); // Simula el tiempo de escaneo
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            initialTime += tiempoEscaneo * 1000; // Convertir segundos a milisegundos
         }
 
-        System.out.println("La cajera ha terminado con el cliente: " + cliente.getId() + " en el tiempo: " + (initialTime / 1000) + " segundos");
-        return initialTime;
+        System.out.println("La cajera " + id + " ha terminado con el cliente: " + cliente.getId()
+                + " en el tiempo: " + ((System.currentTimeMillis() - initialTime) / 1000) + " segundos");
     }
 }
--- compras/src/Main.java
+++ compras/src/Main.java
@@ -1,19 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Cajera cajera1 = new Cajera(1);
+        // Crear los clientes
+        Cliente cliente1 = new Cliente(1, new int[] { 2, 3, 1, 3, 2 }); // Tiempo en segundos
+        Cliente cliente2 = new Cliente(2, new int[] { 5, 2, 1 });       // Tiempo en segundos
 
-        List<Cliente> clientes = new ArrayList<>();
-        clientes.add(new Cliente(1, new int[] { 2, 3, 1, 3, 2 })); // Cliente 1
-        clientes.add(new Cliente(2, new int[] { 5, 2, 1 }));       // Cliente 2
-        clientes.add(new Cliente(3, new int[] { 4, 3 }));          // Cliente 3
+        // Tiempo inicial de referencia
+        long initialTime = System.currentTimeMillis();
 
-        long initialTime = 0;
+        // Crear las cajeras como hilos
+        Cajera cajera1 = new Cajera(1, cliente1, initialTime);
+        Cajera cajera2 = new Cajera(2, cliente2, initialTime);
 
-        for (Cliente cliente : clientes) {
-            initialTime = cajera1.procesarCompra(cliente, initialTime);
-        }
+        // Iniciar los hilos
+        cajera1.start();
+        cajera2.start();
     }
 }
